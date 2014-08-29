388540a695965b46b1fdf8e1f80566b23d54602d
Tim Christensen <ch4nger@gmail.com>
2014-08-29 13:31:39 -0700

    Testing gitignore

--- src/my/concrete/Employee.java
+++ src/my/concrete/Employee.java
@@ -4,19 +4,12 @@
  * and open the template in the editor.
  */
 
-package my;
+package my.concrete;
 
 /**
  *
  * @author Timothy
  */
-public class concrete {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Employee {
     
 }
